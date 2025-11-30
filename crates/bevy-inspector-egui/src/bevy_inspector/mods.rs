97d1870c2498fb9df32a15a09d071251d8c45c22
Jonathan Dickert <jonathan.dickert@protonmail.com>
2025-11-30 15:18:10 -0500

    another fn

--- crates/bevy-inspector-egui/src/bevy_inspector/mods.rs
+++ crates/bevy-inspector-egui/src/bevy_inspector/mods.rs
@@ -16,9 +16,17 @@ use bevy_ecs::{
 use bevy_reflect::TypeRegistry;
 use std::path::Path;
 
-pub type ComponentContextMenu<'f> = &'f mut dyn FnMut(
+pub type EntityComponentContextMenu<'f> = &'f mut dyn FnMut(
     &mut egui::Ui,
     Entity,
+    &mut RestrictedWorldView<'_>, // component_view
+    &mut Context<'_>,
+    &TypeRegistry,
+);
+
+pub type EntitiesComponentContextMenu<'f> = &'f mut dyn FnMut(
+    &mut egui::Ui,
+    &[Entity],
     &mut RestrictedWorldView<'_>, // component_view
     &mut Context<'_>,
     &TypeRegistry,
@@ -29,7 +37,7 @@ pub fn ui_for_entity(
     entity: Entity,
     ui: &mut egui::Ui,
     // BEGIN MOD - allow for custom context menu additions
-    mut mod_context_menu: Option<ComponentContextMenu>,
+    mut mod_context_menu: Option<EntityComponentContextMenu>,
     // END MOD
 ) {
     let type_registry = world.resource::<AppTypeRegistry>().0.clone();
@@ -56,7 +64,7 @@ pub fn ui_for_entity_with_children(
     entity: Entity,
     ui: &mut egui::Ui,
     // BEGIN MOD - allow for custom context menu additions
-    mut mod_context_menu: Option<ComponentContextMenu>,
+    mut mod_context_menu: Option<EntityComponentContextMenu>,
     // END MOD
 ) {
     let type_registry = world.resource::<AppTypeRegistry>().0.clone();
@@ -85,7 +93,7 @@ pub fn ui_for_entity_with_children_inner<F>(
     type_registry: &TypeRegistry,
     filter: &F,
     // BEGIN MOD - allow for custom context menu additions
-    _mod_context_menu: &mut Option<ComponentContextMenu>,
+    _mod_context_menu: &mut Option<EntityComponentContextMenu>,
     // END MOD
 ) where
     F: EntityFilter,
@@ -142,7 +150,7 @@ pub fn ui_for_entity_components(
     id: egui::Id,
     type_registry: &TypeRegistry,
     // BEGIN MOD - allow for custom context menu additions
-    mod_context_menu: &mut Option<ComponentContextMenu>,
+    mod_context_menu: &mut Option<EntityComponentContextMenu>,
     // END MOD
 ) {
     let Ok(components) = components_of_entity(world, entity) else {
@@ -207,7 +215,7 @@ pub fn ui_for_entity_components(
             set_highlight_style(ui);
         }
 
-        let _response = header.show(ui, |ui| {
+        let response = header.show(ui, |ui| {
             ui.reset_style();
 
             let mut env = InspectorUi::for_bevy(type_registry, &mut cx);
@@ -236,11 +244,9 @@ pub fn ui_for_entity_components(
             };
         });
 
-        let response = _response.header_response;
-
         // BEGIN MOD - allow user context menu
-        if let Some(context_menu) = mod_context_menu {
-            response.context_menu(|ui| {
+        response.header_response.context_menu(|ui| {
+            if let Some(context_menu) = mod_context_menu {
                 (context_menu)(ui, entity, &mut component_view, &mut cx, type_registry);
 
                 if let Some(location) = changed_by {
@@ -264,13 +270,140 @@ pub fn ui_for_entity_components(
                         }
                     }
                 }
-            });
-        }
+            }
+        });
         // END MOD
 
         #[cfg(feature = "documentation")]
-        crate::egui_utils::show_docs(response, type_docs);
+        crate::egui_utils::show_docs(response.header_response, type_docs);
 
         ui.reset_style();
     }
+}
+
+pub fn ui_for_entities_shared_components(
+    world: &mut World,
+    entities: &[Entity],
+    ui: &mut egui::Ui,
+    // BEGIN MOD - allow for custom context menu additions
+    mod_context_menu: &mut Option<EntitiesComponentContextMenu>,
+    // END MOD
+) {
+    let type_registry = world.resource::<AppTypeRegistry>().0.clone();
+    let type_registry = type_registry.read();
+
+    let Some(&first) = entities.first() else {
+        return;
+    };
+
+    let Ok(mut components) = components_of_entity(&mut world.into(), first) else {
+        return errors::entity_does_not_exist(ui, first);
+    };
+
+    for &entity in entities.iter().skip(1) {
+        components.retain(|(_, id, _, _)| {
+            world
+                .get_entity(entity)
+                .map_or(true, |entity| entity.contains_id(*id))
+        })
+    }
+
+    let mut queue = CommandQueue::default();
+
+    let id = egui::Id::NULL;
+    for (name, component_id, component_type_id, size) in components {
+        let id = id.with(component_id);
+
+        let header = egui::CollapsingHeader::new(&name).id_salt(id);
+
+        let Some(component_type_id) = component_type_id else {
+            header.show(ui, |ui| errors::no_type_id(ui, &name));
+            continue;
+        };
+
+        #[cfg(feature = "documentation")]
+        let type_docs = type_registry
+            .get_type_info(component_type_id)
+            .and_then(|info| info.docs());
+
+        if size == 0 {
+            ui.indent(id, |ui| {
+                let _response = ui.label(&name);
+                #[cfg(feature = "documentation")]
+                crate::egui_utils::show_docs(_response, type_docs);
+            });
+            continue;
+        }
+
+        let (resources_view, mut components_view) =
+            RestrictedWorldView::resources_components(world);
+        let mut cx = Context {
+            world: Some(resources_view),
+            queue: Some(&mut queue),
+        };
+
+        let mut values = Vec::with_capacity(entities.len());
+        for (i, &entity) in entities.iter().enumerate() {
+            // skip duplicate entities
+            if entities[0..i].contains(&entity) {
+                continue;
+            };
+
+            // SAFETY: entities are distinct, env has a context with just resources
+            match unsafe {
+                components_view.get_entity_component_reflect_unchecked(
+                    entity,
+                    component_type_id,
+                    &type_registry,
+                )
+            } {
+                Ok(value) => {
+                    values.push(value);
+                }
+                Err(error) => {
+                    errors::show_error(error, ui, &name);
+                    return;
+                }
+            }
+        }
+
+        let response = header.show(ui, |ui| {
+            ui.reset_style();
+
+            let mut env = InspectorUi::for_bevy(&type_registry, &mut cx);
+            let id = id.with(component_id);
+            let options = &();
+
+            let mut values_reflect: Vec<_> = values
+                .iter_mut()
+                .map(|value| value.bypass_change_detection().as_partial_reflect_mut())
+                .collect();
+
+            let changed = env.ui_for_reflect_many_with_options(
+                component_type_id,
+                &name,
+                ui,
+                id,
+                options,
+                values_reflect.as_mut_slice(),
+                &|a| a,
+            );
+
+            if changed {
+                for value in values.iter_mut() {
+                    value.set_changed();
+                }
+            }
+        });
+
+        // BEGIN MOD - allow user context menu
+        response.header_response.context_menu(|ui| {
+            if let Some(context_menu) = mod_context_menu {
+                (context_menu)(ui, entities, &mut components_view, &mut cx, &type_registry);
+            }
+        });
+        // END MOD
+    }
+
+    queue.apply(world);
 }
